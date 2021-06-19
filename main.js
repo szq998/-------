d9232b8670cf9a59a2b1d98ee91cb721fda7b8ed
szq998 <szq001@foxmail.com>
2021-06-19 16:59:31 +0800

    $safari.open前，首先退回到rootViewController

--- main.js
+++ main.js
@@ -230,6 +230,6 @@ function renderItem(itemHeight, item) {
 }
 
 function getLinkOpenedInJSBox(url) {
-    const script = `$safari.open('${url}')`;
+    const script = `$objc("UIApplication").$sharedApplication().$delegate().$window().$rootViewController().$dismissViewControllerAnimated_completion(true, $block('void, void',()=>{$safari.open('${url}')}))`;
     return `jsbox://run?script=${encodeURIComponent(script)}`;
 }
